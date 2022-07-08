f80291db527503b025d2f7a8581eb6bcafb775a8
AvgustChen <avchen@yandex.ru>
2022-07-08 13:46:07 +0300

    еще одно редактирование недочетов в задачи 4

--- HomeWork4/Program.cs
+++ HomeWork4/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-Console.WriteLine("Приветствую, данная программа все положительные числа от 1 до Вашего числа");
+Console.WriteLine("Приветствую, данная программа все четные числа от 1 до Вашего числа");
 Console.Write("Введите Ваше число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
@@ -10,7 +10,8 @@ Console.Write("Итого: ");
 if(num > 0)
     while(current <= num)
     {
-        Console.Write(current + " ");
-        current++;
+        if(current % 2 == 0)
+            Console.Write(current + " ");
+            current++;
     }
 else Console.Write("Вы пытаетесь нас обмунуть и ввели отрицательное число!!!");
