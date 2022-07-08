8a5f0aa527ac5f38aaae876de9be1f0fefb36387
AvgustChen <avchen@yandex.ru>
2022-07-08 14:07:27 +0300

    Добавил второе решение через массив

--- HomeWork2/Program.cs
+++ HomeWork2/Program.cs
@@ -26,3 +26,31 @@ else
 {
     Console.Write("Максимальное из этих чисел: " + max);
 }
+
+/*второе решение через массив
+
+Console.WriteLine("Приветствую, данная программа определит какое из ваших чисел больше!");
+int[] num = new int [3];
+Console.Write("Введите первое число: ");
+num[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+num[1] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+num[2] = Convert.ToInt32(Console.ReadLine());
+int temp = num[0];
+int max = num[0];
+int index = 0;
+
+while(index < num.Length)
+{
+    if(num[index] > temp)
+    {
+        max=num[index];
+    }
+index++; 
+}
+Console.Write("Максимальное из этих чисел: " + max);
+*/
+
+
+
