02184cc2af15a7b8f4518c0ff3d0a5b089736ab7
AvgustChen <avchen@yandex.ru>
2022-07-08 13:11:50 +0300

    Добавленно решение 4й задачи

---
+++ HomeWork4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Приветствую, данная программа все положительные числа от 1 до Вашего числа");
+Console.Write("Введите Ваше число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = num / num;
+
+Console.Write("Ваши числа: ");
+while(current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
